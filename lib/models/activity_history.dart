26c9f6a82d8c8ca4ee36ae6ad8b5816c4d1a067a
mingwei <mingwei@uw.edu>
2024-11-26 11:07:18 -0800

    finished activity provider and activity history co

--- lib/models/activity_history.dart
+++ lib/models/activity_history.dart
@@ -1,1 +1,27 @@
-// TODO(Michael): Finish this
+import 'package:go_green/models/entry.dart';
+
+// represents an ActivityHistory
+class ActivityHistory {
+  final List<Entry> _entries;
+
+  ActivityHistory() : _entries = [];
+
+  List<Entry> get entries => List.unmodifiable(_entries);
+
+  ActivityHistory._internal({required List<Entry> entries}) : _entries = List<Entry>.from(entries);
+
+  // gets a clone of ActivityHistory
+  ActivityHistory clone() {
+    return ActivityHistory._internal(entries: List<Entry>.from(_entries));
+  }
+
+  // upsert an entry into _entries, replace if id already exists
+  void upsertEntry(Entry entry) {
+    final index = _entries.indexWhere((entry1) => entry1.id == entry.id);
+    if (index != -1) {
+        _entries[index] = entry;
+    } else {
+      _entries.add(entry);
+    }
+  }
+}
